fbe1676bd4f5d3887ddaa35b434cd463fb3012a6
abewketu21 <abewketu21@gmail.com>
2022-04-19 17:12:10 +0300

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array
+ * @a: A pointer to an int
+ * @n: return value n
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+
+i = 0;
+while (i < 10);
+{
+printf("%d", a[i]);
+
+if (i < n -1)
+{
+printf(", ")
+}
+i++;
+}
+printf("\n")
+}
