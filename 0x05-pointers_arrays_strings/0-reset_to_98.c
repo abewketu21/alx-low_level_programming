8bfeef1a66b8a873871c7ce89ec1bf20f7bbe521
abewketu21 <abewketu21@gmail.com>
2022-04-19 13:07:44 +0300

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
  * reset_to_98 - updates a integer value to 98
  * @n: A pointer to an int that will be updated
  * Return: void that means correct
  */
+
 void reset_to_98(int *n);
+
 {
 *n = 98;
 }
