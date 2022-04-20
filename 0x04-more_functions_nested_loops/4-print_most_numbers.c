356aeba9fc197a40366aff322c2c542358724495
abewketu21 <abewketu21@gmail.com>
2022-04-20 12:01:20 +0300

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,19 +3,18 @@
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  * _putchar prints 0-9, excluding 2 and 4, followed by new line
+ * 
+ * Return: 0-9 excluding 2 and 4
  */
 
 void print_most_numbers(void)
 {
 int i;
 
-i = 0;
-
 for (i = 0; i <= 9; i++)
 {
-if ((i == 2) || (i == 4))
+if (i == 2 || i == 4)
 {
-i++;
 _putchar(i + '0');
 }
 _putchar('\n');
