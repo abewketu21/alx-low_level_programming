30956961894eb46591cc2cd320905395d8555c2d
abewketu21 <abewketu21@gmail.com>
2022-04-20 11:56:04 +0300

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,16 +10,14 @@ void print_most_numbers(void)
 {
 int i;
 
+i = 0;
+
 for (i = 0; i <= 9; i++)
 {
 if ((i == 2) || (i == 4)
 {
-continue;
-}
-else
-{
+i++;
 _putchar(i + '0');
-}
 }
 _putchar('\n');
 }
