3f9fe73dca93e124247341d71172966ab3cca844
abewketu21 <abewketu21@gmail.com>
2022-04-20 10:47:37 +0300

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,10 +10,26 @@
 
 int main(void)
 {
-int num;
+char a[100];
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+int r, n, i;
+
+n = 0;
+i = 0;
+
+srand(time(NULL));
+while (n < 2645)
+{
+r = rand() % 122;
+if (r > 32)
+{
+a[i++] = r;
+n += r;
+}
+}
+a[i++] = (2772 - n);
+a[i] = '\0';
+printf("%s", a);
+
 return (0);
 }
