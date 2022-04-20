691443238529c8bf7577e9df52a5586e69772d82
abewketu21 <abewketu21@gmail.com>
2022-04-20 09:43:52 +0300

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,15 +13,15 @@ void print_array(int *a, int n)
 int i;
 
 i = 0;
-while (i < n);
+while (i < n)
 {
 printf("%d", a[i]);
 
 if (i < n -1)
 {
-printf(", ")
+printf(", ");
 }
 i++;
 }
-printf("\n")
+printf("\n");
 }
