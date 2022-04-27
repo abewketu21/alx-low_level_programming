d0d06e4e296a2b3a2795747e0bffb677f513183d
abewketu21 <abewketu21@gmail.com>
2022-04-27 11:40:55 +0300

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,19 @@
+char *rot13(char *s)
+{
+int x, y;
+
+char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (x = 0; s[x] != '\0'; x++)
+{
+for (y = 0; input[y] != '\0'; y++)
+{
+if (s[x] == input[y])
+{
+s[x] = output[y];
+break;
+}
+}
+}
+return (s);
+}
