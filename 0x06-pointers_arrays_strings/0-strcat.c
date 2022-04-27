adb72b0e95bde9010dde20abb0073049ffddb411
abewketu21 <abewketu21@gmail.com>
2022-04-27 10:58:34 +0300

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /**
  * _strcat - concatenaters two strings
  * @dest: second string to be concatenated to src
- * @src first string to be concatenated to dest
+ * @src: first string to be concatenated to dest
  * Return: Pointer to the resulting string dest
  */
 
@@ -12,13 +12,13 @@ char *_strcat(char *dest, char *src)
 int x, y;
 
 for (x = 0; dest[x] != '\0'; x++)
-;
+{
 for (y = 0; src[y] != '\0'; y++)
 {
 dest[x] = src[y];
 x++;
 }
 dest[x] = '\o';
-
+}
 return (dest);
 }
