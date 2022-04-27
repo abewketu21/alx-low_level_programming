b6e165088f34b91b0ba169e2574bc7f66da9836f
abewketu21 <abewketu21@gmail.com>
2022-04-27 11:05:50 +0300

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenaters two strings
+ * @dest: second string to be concatenated to src
+ * @src: first string to be concatenated to dest
+ * @n: integer that determines number of bytes to the copied
+ * Return: Pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src)
+{
+int x, y;
+
+for (x = 0; dest[x] != '\0'; x++)
+{
+for (y = 0; src[y] != '\0' && n > 0; y++, n--, x++)
+{
+dest[x] = src[y];
+}
+}
+return (dest);
+}
