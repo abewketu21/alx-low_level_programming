1a151bd3322a987d947d86768c18f6a262264953
abewketu21 <abewketu21@gmail.com>
2022-04-27 11:21:07 +0300

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcmp - concatenaters two strings
+@s1: second string to be concatenated to src
+@s2: first string to be concatenated to dest
+Return: Pointer to the resulting string dest
+*/
+
+char *_strcmp(char *s1, char *s2)
+{
+int x;
+
+for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+{
+if (s1[x] != s2[x])
+{
+return (s1[x] - s2[x])
+}
+}
+return (0);
+}
